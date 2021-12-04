2016f2440e47342b08de97938fa2ec224ebbb304
masvsal <95512439+masvsal@users.noreply.github.com>
2021-12-03 18:20:12 -0800

    Add files via upload

---
+++ StringExercise2.py
@@ -0,0 +1,9 @@
+#fix the following code. It should run once you have done this successfully.
+
+"3*8=24
+
+"The red dog was named "rufus" " 
+
+"Good morning\"
+
+"beebo went to the market and said 'hello' to the shopkeep who was named "Jim""
