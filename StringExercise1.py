c917da76dc80ee74cb1c8f3ceb9f6b2f112aa717
masvsal <95512439+masvsal@users.noreply.github.com>
2021-12-03 18:19:40 -0800

    Add files via upload

---
+++ StringExercise1.py
@@ -0,0 +1,11 @@
+#convert the following data to string format
+#the code should run once you have done this successfully
+3 + 3
+
+ILikeDoughnuts!
+
+I am 5 feet 8 inches tall
+
+She said "hello"
+
+The 'product' of "3 and (9*87)^2" is a big number. 
