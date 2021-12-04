f1a614d3b4af45b311b8b86f1cf5b9aa1b1bdbbb
masvsal <95512439+masvsal@users.noreply.github.com>
2021-12-03 18:18:44 -0800

    Add files via upload

---
+++ VariableExercises1.py
@@ -0,0 +1,18 @@
+#assign your name, the grade that you're in, and school that you attend to variables named "age", "grade", and "school" (without the quotation marks). 
+#run the program after you have done so. If you successfully assigned all variables, the program should print a short summary about yourself.
+
+
+#assign variables here:
+#_________________________
+
+
+
+#__________________________
+
+
+def printSummary():
+    print("My name is " + name + ",")
+    print("and I am in grade " + grade + " at " + school + "." )
+
+
+printSummary()
