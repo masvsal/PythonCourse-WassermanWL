c8374a897bb80204c9129c90c3771da69dea4fba
masvsal <95512439+masvsal@users.noreply.github.com>
2021-12-03 18:20:28 -0800

    Add files via upload

---
+++ VariableExercises2.py
@@ -0,0 +1,19 @@
+x  = "3"
+y = "5"
+z = "10"
+
+
+x = "5"
+#what value does x now contain? Please enter your answer below in the form of a string
+x_answer1 = " "
+
+y = x
+#what value do x and y now contain? Please enter your answer below in the form of a string
+y_answer1 = ""
+x_answer2 = ""
+
+
+print(str(bin(ord(x_answer1))) == "0b110101")
+
+
+
